14fa4d445eb81052cecab708cb8443fb3f0533f2
Fawziya20 <fawziya267@gmail.com>
2025-05-08 11:58:35 +0000

    SCRUM-53: Bug login

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import argparse
 import itertools
 from collections import Counter
 from collections import deque
-import streamlit as st
+
 import cv2 as cv
 import numpy as np
 import mediapipe as mp
